83ac778a32a9395c059b93a94cdf6907aa636d4f
virajuk <virajudayakumara@gmail.com>
2022-05-26 13:25:50 -0400

    debugging

--- main.py
+++ main.py
@@ -24,24 +24,20 @@ class Game:
         # tile_count, player_count = 0, 0
         # for sprite in self.level.visible_sprites:
         #     if isinstance(sprite, (Tile, )):
-        #         tile_count += 1
+        #         print(sprite.groups())
         #     if isinstance(sprite, (Player, )):
-        #         player_count += 1
-        #
+        #         print(sprite.groups())
+
         # print(f"tile_count {tile_count}, player_count {player_count}")
 
-        running = False
+        running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
 
             self.screen.fill('#53c653')
-            print(type(self.level.visible_sprites))
-            print(self.level.visible_sprites[0])
-
-            # debug(f"WIDTH {WIDTH}, HEIGHT {HEIGHT}")
-            # self.level.run()
+            self.level.run()
             pygame.display.update()
             self.clock.tick(FPS)
 
